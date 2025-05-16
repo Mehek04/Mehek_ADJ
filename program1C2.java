ce2264739f9163d8ec6deb544eb45f3c02b2f01e
Mehek04 <153306587+Mehek04@users.noreply.github.com>
2025-05-16 14:31:52 -0300

    Add files via upload

---
+++ program1C2.java
@@ -0,0 +1,24 @@
+package assignment;
+import java.util.LinkedList;
+import java.util.Iterator;
+
+public class program1C2 {
+	    public static void main(String[] args) {
+	        LinkedList<String> list = new LinkedList<>();
+	        list.add("Red");
+	        list.add("Green");
+	        list.add("Blue");
+	        list.add("Yellow");
+
+	        System.out.println("Original list: " + list);
+
+	        Iterator<String> descIterator = list.descendingIterator();
+
+	        System.out.print("Reverse order: ");
+	        while (descIterator.hasNext()) {
+	            System.out.print(descIterator.next() + " ");
+	        }
+	        System.out.println();
+	    }
+
+}
