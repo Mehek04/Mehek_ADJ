0b4876cb7c43bdd9ef5a63f37960cb7f2e48b899
Mehek04 <153306587+Mehek04@users.noreply.github.com>
2025-05-16 14:02:47 -0300

    Add files via upload

---
+++ program1b1.java
@@ -0,0 +1,21 @@
+package assignment;
+import java.util.ArrayList;
+public class program1b1 {
+		    public static void main(String[] args) {
+		        ArrayList<String> colors = new ArrayList<>();
+		        colors.add("Red");
+		        colors.add("Green");
+		        colors.add("Blue");
+		        colors.add("Yellow");
+		        System.out.println("Colors: " + colors);
+		        if (colors.contains("Red")) {
+		            System.out.println("The color 'Red' is available in the list.");
+		        } else {
+		            System.out.println("The color 'Red' is NOT available in the list.");
+		        }
+		    }
+		}
+
+	
+
+
