5270713988fbb88204f63294b468e7432ce37c90
Mehek04 <153306587+Mehek04@users.noreply.github.com>
2025-05-16 14:09:40 -0300

    Add files via upload

---
+++ program1b2.java
@@ -0,0 +1,21 @@
+package assignment;
+import java.util.ArrayList;
+public class program1b2 {
+	    public static void main(String[] args) {
+	        ArrayList<String> colors = new ArrayList<>();
+	        colors.add("Red");
+	        colors.add("Green");
+	        colors.add("Blue");
+	        colors.add("Yellow");
+
+	        System.out.println("Original list: " + colors);
+
+	        // Remove 2nd element (index 1)
+	        colors.remove(1);
+
+	        // Remove color "Blue"
+	        colors.remove("Blue");
+
+	        System.out.println("After removal: " + colors);
+	    }
+}
