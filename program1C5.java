7af73c85237b403e4469fc2701a11562d8dc3ad5
Mehek04 <153306587+Mehek04@users.noreply.github.com>
2025-05-16 22:43:58 -0300

    Update program1C5.java

--- program1C5.java
+++ program1C5.java
@@ -1,3 +1,5 @@
+/* 1c5. Write a Java program that swaps two elements ( first and third elements ) in a linked list ( using
+Collections.swap(l_list, 0, 2))*/
 package assignment;
 import java.util.LinkedList;
 import java.util.Collections;
