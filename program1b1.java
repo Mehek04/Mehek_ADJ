1eae112a9aeb93674a462c010f13a25bcca767c1
Mehek04 <153306587+Mehek04@users.noreply.github.com>
2025-05-16 22:36:00 -0300

    Update program1b1.java

--- program1b1.java
+++ program1b1.java
@@ -1,3 +1,5 @@
+/*1b1. Write a java program for getting different colors through ArrayList interface and search whether
+the color "Red" is available or not*/
 package assignment;
 import java.util.ArrayList;
 public class program1b1 {
